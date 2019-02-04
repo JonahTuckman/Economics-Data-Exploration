02f1b6248c821b9493bdf265881ec49034843293
Jonah Tuckman <JonahTuckman@Jonahs-MacBook-Pro.local>
2019-02-04 10:45:19 -0500

    Changed directory structure based on directions of first assignment

---
+++ DataDay1/Scripts/DataDay1.py
@@ -0,0 +1,15 @@
+####################################################################
+# Econ 277: Data Day 1
+# Author: Jonah Tuckman
+# Date: February 4, 2019
+# Location: ~/Desktop/Economics/Economics-Data-Exploration/DataDay1
+# Purpose: Going through process of building python code
+####################################################################
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+
+dataset = pd.read_csv('RawData/DataSet1_AgricFarmVal.csv')
+
