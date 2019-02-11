e41741e1289f4d4226a930e35c8ad90497776e9e
JonahTuckman <jtuckman9@gmail.com>
2019-02-11 11:27:30 -0500

    Setting working directory, reading csv files

---
+++ DataDay2/Scripts/DataDay2.R
@@ -0,0 +1,16 @@
+####################################################################
+# Econ 277: Data Day 2
+# Author: Jonah Tuckman
+# Date: February 11, 2019
+# Location: ~/Desktop/Economics/Economics-Data-Exploration/DataDay2
+# Purpose: Working through data in R, then python if time permits
+####################################################################
+
+
+
+setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay2')
+## This line sets the working directory to the local path on my machine
+rawdata_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay2/RawData'
+
+images_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay2/Images'
+
