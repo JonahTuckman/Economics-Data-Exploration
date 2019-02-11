f7c0c511a54988fe01b33e79355b6d48cdcacea4
JonahTuckman <jtuckman9@gmail.com>
2019-02-11 17:51:24 -0500

    Combined datasets by common columns into farmval_production_suit

--- DataDay2/Scripts/DataDay2.R
+++ DataDay2/Scripts/DataDay2.R
@@ -14,3 +14,14 @@ rawdata_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration
 
 images_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay2/Images'
 
+library(tidyverse)
+install.packages('tidyverse')
+
+countydata <- read.csv('RawData/DataDay1CountyData.csv')
+countydataSub <- subset(countydata, (YEAR >= 1870 & YEAR <= 1900))
+
+AgricCensus <- read.csv('RawData/AgricCensus_1870_1900.csv')
+# Overlaps with countydataSub with YEAR and FIPS
+combined2 <- left_join(x = countydataSub,y = AgricCensus, by = c('YEAR' = 'YEAR'), suffix = c('.x', '.y'))
+landSuit <- read.csv('RawData/suit_wheat.csv')
+farmval_production_suit = left_join(x = combined2, y = landSuit, by = c("FIPS.y" = "fips"))
