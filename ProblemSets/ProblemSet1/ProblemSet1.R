6ed936751d1ce80e9fc9aa162afbe476496acaab
JonahTuckman <jtuckman9@gmail.com>
2019-02-11 20:14:56 -0500

    Added directory for problemSets and problem set 1, added script for PS1 and imported data

---
+++ ProblemSets/ProblemSet1/ProblemSet1.R
@@ -0,0 +1,12 @@
+####################################################################
+# Econ 277: ProblemSet1
+# Author: Jonah Tuckman
+# Date: February 11, 2019
+# Location: ~/Desktop/Economics/Economics-Data-Exploration/ProblemSets/ProblemSet1
+# Purpose: Working through data in R, then python if time permits
+####################################################################
+
+setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/ProblemSets/ProblemSet1')
+## This line sets the working directory to the local path on my machine
+
+dataset <- read.csv('CountyRRData.csv')
