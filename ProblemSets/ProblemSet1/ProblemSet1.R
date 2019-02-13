b3b70e334de08654ced4d86ad4ba9801ddb06c90
JonahTuckman <jtuckman9@gmail.com>
2019-02-13 10:52:56 -0500

    Finished Part 2

--- ProblemSets/ProblemSet1/ProblemSet1.R
+++ ProblemSets/ProblemSet1/ProblemSet1.R
@@ -14,11 +14,36 @@ agrData <- read.csv('data/AgricCensus_1870_1900.csv')
 
 combinedDataSet = left_join(x = agrData, y = dataset, by = c("FIPS" = "fips"))
 
-## PART1 ##
+## PART 1:Dataset Construction ##
 
 # Question 1: What is a unit of observation in the data?
 ### A unit of observation in the data is kilometers in terms of rail road distance. 
 
 
 combinedDataSet[, 'RR?'] <- ifelse(combinedDataSet['RRinitialtotaldist'] == 0, 0, 1)
+
+## Part 2: Summary Statistics ##
+
+meanFarmVal = mean(combinedDataSet$FARMS, na.rm = TRUE)
+
+noRR = subset(combinedDataSet,`RR?` == 0)
+numberNoRR = nrow(noRR)
+total = nrow(combinedDataSet)
+percentNoRailRoad = (numberNoRR / total) * 100
+
+avgRRKM = mean(combinedDataSet$RRinitialtotaldist, na.rm = TRUE)
+
+standardDevRRKM = sd(combinedDataSet$RRinitialtotaldist , na.rm=TRUE)
+
+#Question 4: What is the mean inflation adjusted farm value?
+print(meanFarmVal)
+#Question 5: What percent of counties did not receive any railroad?
+print(percentNoRailRoad)
+#Question 6: What is the average kms of railroad?
+print(avgRRKM)
+#Question 7: What is the standard deviation of the railroad kms?
+print(standardDevRRKM)
+
+
+## Part 3: Scatterplots with Binary RR Treatment ## 
 
