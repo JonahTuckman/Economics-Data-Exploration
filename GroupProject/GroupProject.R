b4f58ecdf10db419da825720b6570cd8f4c6ce2d
JonahTuckman <jtuckman9@gmail.com>
2019-03-06 10:11:40 -0500

    Mean and Standard DEviation for High and Low income

--- GroupProject/GroupProject.R
+++ GroupProject/GroupProject.R
@@ -31,8 +31,8 @@ write.csv(CaliData, "CaliData.csv")
 
 toMatch <- c("San Francisco" , "San Mateo" , "Santa Clara" , "Alameda" , "Contra Costa" , "Solano" ,
              "Napa" , "Sonoma" , "Marin", "Los Angeles", "Orange County", "San Diego")
-CaliHighTech <- data.frame(CaliData)
-CaliHighTech <- subset(CaliHighTech, grepl(paste(toMatch, collapse="|"), State.and.County))
+CaliHighIncome <- data.frame(CaliData)
+CaliHighIncome <- subset(CaliHighIncome, grepl(paste(toMatch, collapse="|"), State.and.County))
 
 CaliLowIncome <- data.frame(CaliData)
 CaliLowIncome <- subset(CaliLowIncome, !grepl(paste(toMatch, collapse="|"), State.and.County))
@@ -48,17 +48,27 @@ CaliLowIncome$X1959 <- replaceCommas(CaliLowIncome$X1959)
 CaliLowIncome$X1969 <- replaceCommas(CaliLowIncome$X1969)
 CaliLowIncome$X1979 <- replaceCommas(CaliLowIncome$X1979)
 CaliLowIncome$X1989 <- replaceCommas(CaliLowIncome$X1989)
+CaliLowIncome$State.and.County <- NULL
 
 write.csv(CaliLowIncome, "CaliLowIncome.csv")
- 
+
+## Removal of high income commas
+CaliHighIncome$State.and.County <- NULL
+CaliHighIncome$X1959 <- replaceCommas(CaliHighIncome$X1959)
+CaliHighIncome$X1969 <- replaceCommas(CaliHighIncome$X1969)
+CaliHighIncome$X1979 <- replaceCommas(CaliHighIncome$X1979)
+CaliHighIncome$X1989 <- replaceCommas(CaliHighIncome$X1989)
+
+write.csv(CaliHighIncome, "CaliHighTech2.csv")
 ### Removed Commas from numbers here to avoid importation as type factor
 ### Opened in text file, deleted commas, reopened in numbers and saved as CaliHighTech2.csv
 
-CaliHighIncome <- read.csv("CaliHighTech2.csv")
+
 CaliHighIncome$X <- NULL ## Adds X column of index from original list. Not needed
 CaliHighIncome$State.and.County <- NULL ## No names (categorical variables suck to deal with)
-summary(CaliHighIncome)
+summary(CaliHighIncome) # Checking out data
 
+## Checking that everything is going well.
 sapply(CaliHighIncome, class) ## Shows breakdown of columns by type
 
 
@@ -66,14 +76,31 @@ all(duplicated(CaliHighIncome$X1979)[-1L]) ## Test for constant vector
 # Returns false which is good 
 
 
+#### HIGH Income #### 
+
 # Means of each year
-averageHighTech59 = mean(CaliHighIncome$X1959, na.rm = TRUE)
-averageHighTech69 = mean(CaliHighIncome$X1969, na.rm = TRUE)
-averageHighTech79 = mean(CaliHighIncome$X1979, na.rm = TRUE)
-averageHighTech89 = mean(CaliHighIncome$X1989, na.rm = TRUE)
+averageHighIncome59 = mean(CaliHighIncome$X1959, na.rm = TRUE)
+averageHighIncome69 = mean(CaliHighIncome$X1969, na.rm = TRUE)
+averageHighIncome79 = mean(CaliHighIncome$X1979, na.rm = TRUE)
+averageHighIncome89 = mean(CaliHighIncome$X1989, na.rm = TRUE)
 
 #Standard deviation of each year
-StandardDev59 <- sd(CaliHighIncome$X1959, na.rm = TRUE)
-StandardDev69 <- sd(CaliHighIncome$X1969, na.rm = TRUE)
-StandardDev79 <- sd(CaliHighIncome$X1979, na.rm = TRUE)
-StandardDev89 <- sd(CaliHighIncome$X1989, na.rm = TRUE)
+StandardDevHigh59 <- sd(CaliHighIncome$X1959, na.rm = TRUE)
+StandardDevHigh69 <- sd(CaliHighIncome$X1969, na.rm = TRUE)
+StandardDevHigh79 <- sd(CaliHighIncome$X1979, na.rm = TRUE)
+StandardDevHigh89 <- sd(CaliHighIncome$X1989, na.rm = TRUE)
+
+
+#### LOW Income #### 
+
+# Means of each year
+averageLowIncome59 = mean(CaliLowIncome$X1959, na.rm = TRUE)
+averageLowIncome69 = mean(CaliLowIncome$X1969, na.rm = TRUE)
+averageLowIncome79 = mean(CaliLowIncome$X1979, na.rm = TRUE)
+averageLowIncome89 = mean(CaliLowIncome$X1989, na.rm = TRUE)
+
+#Standard deviation of each year
+StandardDevLow59 <- sd(CaliLowIncome$X1959, na.rm = TRUE)
+StandardDevLow69 <- sd(CaliLowIncome$X1969, na.rm = TRUE)
+StandardDevLow79 <- sd(CaliLowIncome$X1979, na.rm = TRUE)
+StandardDevLow89 <- sd(CaliLowIncome$X1989, na.rm = TRUE)
