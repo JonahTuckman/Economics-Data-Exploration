984fcc684871efabd64b865b321b91a801410a1d
JonahTuckman <jtuckman9@gmail.com>
2019-03-25 09:52:41 -0400

    Need to decide necesary variables to keep. Stepping away at the moment

--- DataDays/DataDay3/Scripts/DataDay3.R
+++ DataDays/DataDay3/Scripts/DataDay3.R
@@ -8,7 +8,22 @@
 
 setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDays/DataDay3')
 ## This line sets the working directory to the local path on my machine
-rawdata_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay1/RawData'
+rawdata_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDays/DataDay3/RawData'
 
-images_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay1/Images'
+images_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDays/DataDay3/Images'
+
+install.packages("tidyverse", dependencies =TRUE)
+install.packages("DBI", dependencies = TRUE)
+library(DBI)
+install.packages("dplyr")
+library(tidyverse)
+library(dplyr)
+
+
+dataset <- read.csv("RawData/linked_1880_1920_males.csv")
+serial26500 <-  subset(dataset, (dataset$serial_2 == 6501))
+
+
+
+
 
