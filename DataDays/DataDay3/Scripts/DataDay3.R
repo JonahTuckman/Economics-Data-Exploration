1711609fdcf525a17803abdcfd930ccff0109e84
JonahTuckman <jtuckman9@gmail.com>
2019-03-25 09:34:17 -0400

    added abstraction directories

--- DataDays/DataDay3/Scripts/DataDay3.R
+++ DataDays/DataDay3/Scripts/DataDay3.R
@@ -6,7 +6,7 @@
 ####################################################################
 
 
-setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay1')
+setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDays/DataDay3')
 ## This line sets the working directory to the local path on my machine
 rawdata_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/DataDay1/RawData'
 
