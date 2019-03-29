464f73527265faeb2a3e70b26075725f0a6550c0
JonahTuckman <jtuckman9@gmail.com>
2019-03-29 12:09:41 -0400

    problem set 2

---
+++ ProblemSets/ProblemSet2/ProblemSet2.R
@@ -0,0 +1,15 @@
+####################################################################
+# Econ 277: ProblemSet2
+# Author: Jonah Tuckman
+# Date: February 11, 2019
+# Location: ~/Desktop/Economics/Economics-Data-Exploration/ProblemSets/ProblemSet2
+# Purpose: Working through data in R, then python if time permits
+####################################################################
+
+setwd('/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/ProblemSets/ProblemSet2')
+## This line sets the working directory to the local path on my machine
+images_dir <- '/Users/JonahTuckman/Desktop/Economics/Economics-Data-Exploration/ProblemSet2/Images'
+
+
+#### Importing needed datasets
+dataset <- read.csv('data/linked_1880_1920_males.csv') 
