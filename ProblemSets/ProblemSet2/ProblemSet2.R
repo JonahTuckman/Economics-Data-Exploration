dde82bfb919282e1b1ef36f33cb486319ba46f3c
JonahTuckman <jtuckman9@gmail.com>
2019-03-29 15:11:51 -0400

    eliminating parents

--- ProblemSets/ProblemSet2/ProblemSet2.R
+++ ProblemSets/ProblemSet2/ProblemSet2.R
@@ -34,3 +34,24 @@ dataset <- transform(dataset, child = ifelse (dataset$relate_1 == 'child',1,0))
 dataset <- subset(dataset, (dataset$linktype == 'primary link' | dataset$relate_1 == 'head/householder'))
 
 
+
+### Question 1: 
+# There are 20413 observations in my dataset after doing this. 
+# I have variables to indicate children and fathers and they seem to be trading (0,1,0,1...) which is a good sign
+
+### Question 2: How many siblings do you have in? (Hint these are people that share thesame serial_1)
+# there are no siblings as we have solely kept primary link children and fathers
+
+####### Assign occscore of father to child
+dataset <- merge(dataset, dataset[dataset$father == 1, c("serial_1", "occscore_1")], by = "serial_1")
+
+
+### Now we can drop fathers from the dataset
+if (dataset$father == 1) {
+  
+}
+
+dataset[dataset$father == '1',] <- NA
+dataset <- na.omit(dataset)
+
+
